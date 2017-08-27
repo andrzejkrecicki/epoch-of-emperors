490796482cd720c265740353c7f765e7368a43cb
Andrzej Krecicki <6428958+andrzejkrecicki@users.noreply.github.com>
2017-08-27 23:26:40 +0200

    RandomMapMenu: defining players count and choosing civilizations.

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
 import { TextButton, Header, Label, DropDown } from './ui.js';
+import { PlayerDefinition, PLAYER_COLOURS, CIVILIZATIONS, CIVILIZATIONS_NAMES } from './utils.js';
 
 class Menu extends Konva.Group {
     constructor(stage, nav) {
@@ -103,10 +104,15 @@ class SinglePlayerMenu extends Menu {
 class RandomMapMenu extends Menu {
     constructor(stage, nav) {
         super(...arguments);
-
+        this.game_definition = {
+            players: this.getDefaultPlayersDefinitions()
+        };
+        this.initializeWidgets();
+    }
+    initializeWidgets() {
         this.cancelBtn = new TextButton(
-            stage.width() - 80 - 300,
-            stage.height() - 15 - TextButton.DEFAULT_RECT_OPTIONS.height,
+            this.stage.width() - 80 - 300,
+            this.stage.height() - 15 - TextButton.DEFAULT_RECT_OPTIONS.height,
             { text: 'Cancel' },
             { width: 300 }
         );
@@ -114,7 +120,7 @@ class RandomMapMenu extends Menu {
 
         this.startGameBtn = new TextButton(
             80,
-            stage.height() - 15 - TextButton.DEFAULT_RECT_OPTIONS.height,
+            this.stage.height() - 15 - TextButton.DEFAULT_RECT_OPTIONS.height,
             { text: 'Start Game' },
             { width: 300 }
         );
@@ -132,7 +138,7 @@ class RandomMapMenu extends Menu {
         }));
         row_offset += RandomMapMenu.CIV_SECTION_WIDTH;
         this.add(new Header({
-            text: 'Player',
+            text: 'Colour',
             x: row_offset, y: 80
         }));
         row_offset += RandomMapMenu.PLAYER_SECTION_WIDTH;
@@ -141,27 +147,81 @@ class RandomMapMenu extends Menu {
             x: row_offset, y: 80
         }));
 
+        this.playersSection = new Konva.Group({
+            x: 30,
+            y: 120
+        });
+        this.add(this.playersSection);
+        this.initialziePlayersSection();
+
 
         this.add(new Header({
-            x: 30, y: stage.height() - 220,
+            x: 30, y: this.stage.height() - 220,
             text: 'Number of Players'
         }));
         this.numOfPlayers = new DropDown(
-            30, stage.height() - 200, 80,
+            30, this.stage.height() - 200, 80,
             [2, 3, 4, 5, 6], 2
         );
         this.add(this.numOfPlayers);
-
-
+        this.numOfPlayers.on("update", () => {
+            let old_val = this.game_definition.players.length;
+            let new_val = this.numOfPlayers.chosenIndex + 1;
+            if (old_val > new_val) {
+                this.game_definition.players = this.game_definition.players.slice(
+                    0, new_val + 1
+                );
+            } else {
+                for (let i = old_val; i <= new_val; ++i ) {
+                    this.game_definition.players.push(new PlayerDefinition(i, `Computer ${i}`, null, PLAYER_COLOURS[i], null, true));
+                }
+            }
+            this.playersSection.removeChildren();
+            this.initialziePlayersSection();
+            this.fire("refresh");
+        });
 
         this.cancelBtn.on("click", () => {
             this.navigator.navigate("SinglePlayerMenu");
         });
     }
+    initialziePlayersSection() {
+        var offset = {
+            x: 0, y: 0
+        }
+        for (let i = 0; i < this.game_definition.players.length; ++i) {
+            offset.x = 0;
+
+            this.playersSection.add(new Label({
+                text: this.game_definition.players[i].name,
+                x: offset.x, y: offset.y
+            }));
+
+            offset.x += RandomMapMenu.NAME_SECTION_WIDTH;
+
+            let civDropDown = new DropDown(
+                offset.x, offset.y , 150, 
+                CIVILIZATIONS_NAMES, this.game_definition.players[i].civ
+            );
+            civDropDown.options.setZIndex(10);
+            this.playersSection.add(civDropDown);
+
+            offset.y += 40;
+        }
+    }
+    getDefaultPlayersDefinitions() {
+        return [
+            new PlayerDefinition(0, "You", null, PLAYER_COLOURS[0], null),
+            new PlayerDefinition(1, "Computer 1", null, PLAYER_COLOURS[1], null, true),
+            new PlayerDefinition(2, "Computer 2", null, PLAYER_COLOURS[2], null, true),
+            new PlayerDefinition(3, "Computer 3", null, PLAYER_COLOURS[3], null, true)
+        ];
+    }
 }
 RandomMapMenu.NAME_SECTION_WIDTH = 170;
 RandomMapMenu.CIV_SECTION_WIDTH = 170;
 RandomMapMenu.PLAYER_SECTION_WIDTH = 90;
+RandomMapMenu.DEFAULT_PLAYERS_NUMBER = 4;
 
 
 export {
