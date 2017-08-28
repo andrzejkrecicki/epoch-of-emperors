50ba20e8041817796980edb96588712d6a72ac4b
Andrzej Kręcicki <6428958+andrzejkrecicki@users.noreply.github.com>
2017-08-28 12:34:26 +0200

    Optimize map generating by directly using canvas rendering context.

--- src/viewer.js
+++ src/viewer.js
@@ -27,37 +27,33 @@ class MapDrawable extends Konva.Group {
         this.insertTiles();
     }
     insertTiles() {
+        var tmpCanvas = document.createElement("canvas");
+        tmpCanvas.setAttribute("width", Map.SIZES[this.map.definition.size] * MapDrawable.TILE_SIZE.width);
+        tmpCanvas.setAttribute("height", Map.SIZES[this.map.definition.size] * MapDrawable.TILE_SIZE.height);
+        var tmpCtx = tmpCanvas.getContext('2d');
+
         for (let y = 0; y < Map.SIZES[this.map.definition.size]; ++y) {
             let origin = {
                 x: y * MapDrawable.TILE_COL_OFFSET.x,
                 y: -(Map.SIZES[this.map.definition.size] * MapDrawable.TILE_ROW_OFFSET.y) + (y * MapDrawable.TILE_COL_OFFSET.y)
             };
             for (let x = 0; x < Map.SIZES[this.map.definition.size]; ++x) {
-                var tile;
                 if (this.map.terrain_tiles[x][y] == 1) {
-                    tile = new Konva.Image({
-                        x: origin.x,
-                        y: origin.y,
-                        image: images.grass_00,
-                        width: MapDrawable.TILE_SIZE.width,
-                        height: MapDrawable.TILE_SIZE.height,
-                    });
+                    tmpCtx.drawImage(images.grass_00, origin.x, origin.y);
                 } else if (this.map.terrain_tiles[x][y] == 2) {
-                    tile = new Konva.Image({
-                        x: origin.x,
-                        y: origin.y,
-                        image: images.sand_00,
-                        width: MapDrawable.TILE_SIZE.width,
-                        height: MapDrawable.TILE_SIZE.height,
-                    });
+                    tmpCtx.drawImage(images.sand_00, origin.x, origin.y);
                 }
-                tile.transformsEnabled('position');
-                tile.listening(false);
-                this.add(tile);
                 origin.x += MapDrawable.TILE_ROW_OFFSET.x;
                 origin.y += MapDrawable.TILE_ROW_OFFSET.y;
             }
         }
+        this.add(new Konva.Image({
+            x: 0,
+            y: 0,
+            image: tmpCanvas,
+            width: Map.SIZES[this.map.definition.size] * MapDrawable.TILE_SIZE.width,
+            height: Map.SIZES[this.map.definition.size] * MapDrawable.TILE_SIZE.height
+        }));
         this.cache();
     }
 }
