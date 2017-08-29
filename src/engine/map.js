e965b1dda9fed01904d38e037ece794c2e911d91
Andrzej Krecicki <6428958+andrzejkrecicki@users.noreply.github.com>
2017-08-29 16:06:27 +0200

    BFS moved from RandomMap to separate class and module.

--- src/engine/map.js
+++ src/engine/map.js
@@ -1,4 +1,5 @@
 import Konva from 'konva';
+import { BFSWalker, MultiSlotQueue } from './algorithms.js';
 
 class Map {
     constructor(definition) {
@@ -44,66 +45,35 @@ class RandomMap extends Map {
         let total_surface = RandomMap.SIZES[this.definition.size] * RandomMap.SIZES[this.definition.size];
         // 60% - 80% of land
         let desired_land_surface = Math.floor(total_surface * (Math.random() * 2 + 6) / 10);
-        let flood_fill_seed = {
+        let current_surface = 1;
+
+        let seed = {
             x: Math.floor(RandomMap.SIZES[this.definition.size] / 2),// + Math.random() * 30 - 60),
             y: Math.floor(RandomMap.SIZES[this.definition.size] / 2),// + Math.random() * 30 - 60),
             terrain: Map.TERRAIN_TILES.GRASS
         }
 
-        let queue = new MultiSlotQueue();
-        queue.push(flood_fill_seed);
-
-        var that = this;
-        let visited = {
-            get: function(x, y) {
-                if (this[x + ";" + y]) return true;
-                if (
-                    x >= RandomMap.SIZES[that.definition.size] - 2 || 
-                    x < 2 ||
-                    y >= RandomMap.SIZES[that.definition.size] - 2 ||
-                    y < 2
-                ) return true;
-                return false;
-            },
-            set: function(x, y) {
-                this[x + ";" + y] = true;
-            }
-        };
-        visited.set(flood_fill_seed);
-
-
-        let current_surface = 1;
-        while (!(queue.empty() || current_surface > desired_land_surface)) {
-            let tile = queue.pop();
-
-            for (let x = tile.x - 2; x <= tile.x + 2; ++x)
-                for (let y = tile.y - 2; y <= tile.y + 2; ++y) {
-                    if (Math.abs(x - tile.x) + Math.abs(y - tile.y) != 4) this.terrain_tiles[x][y] = tile.terrain;
+        let that = this;
+        let walker = new BFSWalker(seed, new MultiSlotQueue(), function(tile) {
+                tile.terrain = that.mutateTerrain(tile.terrain);
+                for (let x = tile.x - 2; x <= tile.x + 2; ++x)
+                    for (let y = tile.y - 2; y <= tile.y + 2; ++y) {
+                        if (Math.abs(x - tile.x) + Math.abs(y - tile.y) != 4) that.terrain_tiles[x][y] = tile.terrain;
+                    }
+            }, function(x, y, tile) {
+                ++current_surface;
+                return {
+                    x: x,
+                    y: y,
+                    terrain: tile.terrain
                 }
-
-            if (!visited.get(tile.x - 1, tile.y)) {
-                visited.set(tile.x - 1, tile.y);
-                queue.push({ x: tile.x - 1, y: tile.y, terrain: this.randomizedTileTerrain(tile.terrain) });
-                ++current_surface;
-            }
-            if (!visited.get(tile.x + 1, tile.y)) {
-                visited.set(tile.x + 1, tile.y);
-                queue.push({ x: tile.x + 1, y: tile.y, terrain: this.randomizedTileTerrain(tile.terrain) });
-                ++current_surface;
-            }
-            if (!visited.get(tile.x, tile.y - 1)) {
-                visited.set(tile.x, tile.y - 1);
-                queue.push({ x: tile.x, y: tile.y - 1, terrain: this.randomizedTileTerrain(tile.terrain) });
-                ++current_surface;
-            }
-            if (!visited.get(tile.x, tile.y + 1)) {
-                visited.set(tile.x, tile.y + 1);
-                queue.push({ x: tile.x, y: tile.y + 1, terrain: this.randomizedTileTerrain(tile.terrain) });
-                ++current_surface;
-            }
-        }
+            }, function() {
+                return current_surface > desired_land_surface
+            }, 2, RandomMap.SIZES[this.definition.size] - 2 - 1
+        );
+        walker.run();
     }
-    randomizedTileTerrain(terrain, prob = .0275) {
+    mutateTerrain(terrain, prob = .0275) {
         if (Math.random() < prob) {
             if (terrain == Map.TERRAIN_TILES.GRASS) return Map.TERRAIN_TILES.SAND;
             else return Map.TERRAIN_TILES.GRASS;
@@ -121,38 +91,6 @@ class RandomMap extends Map {
         }
     }
 };
-
-class MultiSlotQueue {
-    constructor(numOfSlots=5) {
-        this.numOfSlots = numOfSlots;
-        this.slots = [];
-        for (let i = 0; i < this.numOfSlots; ++i) {
-            this.slots.push({
-               head: 0,
-               values: []
-            });
-        }
-    }
-    empty() {
-        for (let i = 0; i < this.numOfSlots; ++i) {
-            if (this.slots[i].values.length > this.slots[i].head) return false;
-        }
-        return true;
-    }
-    push(value) {
-        var num = Math.floor(Math.random() * this.numOfSlots);
-        this.slots[num].values.push(value);
-    }
-    pop() {
-        for (let i = 0; i < this.numOfSlots; ++i) {
-            if (this.slots[i].values.length > this.slots[i].head) {
-                return this.slots[i].values[this.slots[i].head++];
-            }
-        }
-        throw new Error();
-    }
-
-}
 
 
 export {
