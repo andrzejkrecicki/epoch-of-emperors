0cc6050b22955bb6b8917aafb965123d938801c7
Andrzej Krecicki <6428958+andrzejkrecicki@users.noreply.github.com>
2017-09-13 23:45:20 +0200

    Normalizing adjecent grass-sand tiles. New bitmaps.

--- src/viewer.js
+++ src/viewer.js
@@ -106,6 +106,23 @@ images[Map.TERRAIN_TYPES.WATERSAND_3] = make_image("img/tiles/watersand_3.png");
 images[Map.TERRAIN_TYPES.WATERSAND_9] = make_image("img/tiles/watersand_9.png");
 
 
+images[Map.TERRAIN_TYPES.GRASSSAND_1] = make_image("img/tiles/grasssand_1.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_2] = make_image("img/tiles/grasssand_2.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_3] = make_image("img/tiles/grasssand_3.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_4] = make_image("img/tiles/grasssand_4.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_6] = make_image("img/tiles/grasssand_6.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_7] = make_image("img/tiles/grasssand_7.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_8] = make_image("img/tiles/grasssand_8.png");
+images[Map.TERRAIN_TYPES.GRASSSAND_9] = make_image("img/tiles/grasssand_9.png");
+
+
+images[Map.TERRAIN_TYPES.SANDGRASS_0] = make_image("img/tiles/sandgrass_0.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_2] = make_image("img/tiles/sandgrass_2.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_2_8] = make_image("img/tiles/sandgrass_2_8.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_4] = make_image("img/tiles/sandgrass_4.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_4_6] = make_image("img/tiles/sandgrass_4_6.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_6] = make_image("img/tiles/sandgrass_6.png");
+images[Map.TERRAIN_TYPES.SANDGRASS_8] = make_image("img/tiles/sandgrass_8.png");
 
 
 
