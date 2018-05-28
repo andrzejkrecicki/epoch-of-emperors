cdd41aae9c70bdd48d1844bf20a91ffd3f6c50dc
Andrzej Kręcicki <6428958+andrzejkrecicki@users.noreply.github.com>
2018-05-28 22:51:06 +0200

    Clubman - support for dying, attacking. Adjusted offsets.

--- src/engine/units/clubman.js
+++ src/engine/units/clubman.js
@@ -1,7 +1,9 @@
 import { Unit } from './unit.js';
+import { Building } from '../buildings/building.js';
 import { Sprites } from '../../sprites.js';
 import { TERRAIN_TYPES } from '../terrain.js';
 import { Actions } from '../actions.js';
+import * as interactions from '../interactions.js';
 
 class ClubMan extends Unit {
     constructor() {
@@ -10,6 +12,9 @@ class ClubMan extends Unit {
             attack: ClubMan.prototype.ATTRIBUTES.ATTACK,
             armor: ClubMan.prototype.ATTRIBUTES.ARMOR
         }
+    }
+    getInteractionType(object) {
+        if (object instanceof Unit || object instanceof Building) return interactions.AttackInteraction;
     }
     get ACTIONS() {
         if (this.state & Unit.prototype.STATE.IDLE) return [
@@ -23,6 +28,7 @@ ClubMan.prototype.AVATAR = Sprites.Sprite("img/interface/avatars/clubman.png");
 ClubMan.prototype.MAX_HP = 40;
 ClubMan.prototype.SPEED = 1;
 ClubMan.prototype.CREATION_TIME = 26 * 35;
+ClubMan.prototype.ATTACK_RATE = 10 * 3;
 
 ClubMan.prototype.ACTION_KEY = "T";
 ClubMan.prototype.COST = {
@@ -35,15 +41,24 @@ ClubMan.prototype.ATTRIBUTES = {
     ARMOR: 0
 }
 
+ClubMan.prototype.FRAME_RATE = Object.assign({}, Unit.prototype.FRAME_RATE);
+ClubMan.prototype.FRAME_RATE[ClubMan.prototype.STATE.ATTACK] = 3;
+
+
 ClubMan.prototype.IMAGES = {
-    [ClubMan.prototype.STATE.IDLE]: Sprites.DirectionSprites("img/units/clubman/idle/", 1),
-    [ClubMan.prototype.STATE.MOVING]: Sprites.DirectionSprites("img/units/clubman/moving/", 15)
+    [ClubMan.prototype.STATE.IDLE]: Sprites.DirectionSprites("img/units/clubman/idle/", 6),
+    [ClubMan.prototype.STATE.MOVING]: Sprites.DirectionSprites("img/units/clubman/moving/", 15),
+    [ClubMan.prototype.STATE.ATTACK]: Sprites.DirectionSprites("img/units/clubman/attack/", 15),
+    [ClubMan.prototype.STATE.DYING]: Sprites.DirectionSprites("img/units/clubman/dying/", 10),
+    [ClubMan.prototype.STATE.DEAD]: Sprites.DirectionSprites("img/units/clubman/dead/", 6),
 };
 
 ClubMan.prototype.IMAGE_OFFSETS = {
-    [ClubMan.prototype.STATE.IDLE]: { x: 6, y: 34 },
-    [ClubMan.prototype.STATE.MOVING]: { x: 6, y: 33 },
+    [ClubMan.prototype.STATE.IDLE]: { x: 3, y: 34 },
+    [ClubMan.prototype.STATE.MOVING]: { x: 2, y: 34 },
+    [ClubMan.prototype.STATE.ATTACK]: { x: 27, y: 45 },
+    [ClubMan.prototype.STATE.DYING]: { x: 50, y: 40 },
+    [ClubMan.prototype.STATE.DEAD]: { x: 55, y: 29 },
 };
-
 
 export { ClubMan }
